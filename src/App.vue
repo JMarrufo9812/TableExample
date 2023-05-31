dc918136e3ce091ab0c7e4c8a76bcc201a2b86f7
Jmarrufo <eliot@cervusconsulting.com>
2023-05-31 14:26:43 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <b-table striped hover :items="items" :fields="fields"></b-table>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { BTable } from 'bootstrap-vue';
+
+const items = ref([
+  { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+  { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+  { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+  { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
+]);
+
+const fields = ref([
+  {
+    key: 'last_name',
+    sortable: true
+  },
+  {
+    key: 'first_name',
+    sortable: false
+  },
+  {
+    key: 'age',
+    label: 'Person age',
+    sortable: true,
+    variant: 'danger'
+  }
+]);
+</script>
+
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { createApp  } from 'vue'
+import App from './App.vue'
+import '@vue/compat';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+const app = createApp(App);
+
+app.mount('#app');
